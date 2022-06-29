2e018a546642d4e8f3ee84316f7f43e5c6a177b2
Matrix <2513084541@qq.com>
2022-06-29 20:04:16 +0800

    add vulkan cmake

---
+++ CompileVulkanShader.cmake
@@ -0,0 +1,22 @@
+function (add_shaders_target TARGET)
+  cmake_parse_arguments ("SHADER" "" "SHADER_OUTPUT_DIR" "SOURCES" ${ARGN})
+  set (SHADERS_DIR ${SHADER_SHADER_OUTPUT_DIR}/Shader)
+#   message(STATUS SHADER_SOURCES = ${SHADER_SOURCES})
+  add_custom_command (
+    OUTPUT ${SHADERS_DIR}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${SHADERS_DIR}
+    )
+  add_custom_command (
+    OUTPUT ${SHADERS_DIR}/frag.spv ${SHADERS_DIR}/vert.spv
+    COMMAND glslang::validator
+    ARGS --target-env vulkan1.0 ${SHADER_SOURCES} --quiet
+    WORKING_DIRECTORY ${SHADERS_DIR}
+    DEPENDS ${SHADERS_DIR} ${SHADER_SOURCES}
+    COMMENT "Compiling Shaders"
+    VERBATIM
+    )
+  add_custom_target (${TARGET} DEPENDS ${SHADERS_DIR}/frag.spv ${SHADERS_DIR}/vert.spv)
+  if(TARGET ${TARGET})
+    message(STATUS  "${TARGET} Shaders has compile sucessfully"  )
+  endif()
+endfunction ()
