3c32f1a15dde350a6346614f7466e4a6c7c45dce
Matrix <2513084541@qq.com>
2022-07-06 20:54:31 +0800

    update

--- CompileVulkanShader.cmake
+++ CompileVulkanShader.cmake
@@ -1,7 +1,9 @@
 function (add_shaders_target TARGET)
-  cmake_parse_arguments ("SHADER" "" "SHADER_OUTPUT_DIR" "SOURCES" ${ARGN})
-  set (SHADERS_DIR ${SHADER_SHADER_OUTPUT_DIR}/Shader)
-#   message(STATUS SHADER_SOURCES = ${SHADER_SOURCES})
+  set(option TEST_Option)
+  set(oneValueArgs OUTPUT_DIR TARGET_OUTPUT_NAME )
+  cmake_parse_arguments ("SHADER" "" "${oneValueArgs}" "SOURCES" ${ARGN})
+  set (SHADERS_DIR ${SHADER_OUTPUT_DIR}/Shader)
+  # message(STATUS OUTPUT_DIR = ${SHADERS_DIR})
   add_custom_command (
     OUTPUT ${SHADERS_DIR}
     COMMAND ${CMAKE_COMMAND} -E make_directory ${SHADERS_DIR}
@@ -9,7 +11,7 @@ function (add_shaders_target TARGET)
   add_custom_command (
     OUTPUT ${SHADERS_DIR}/frag.spv ${SHADERS_DIR}/vert.spv
     COMMAND glslang::validator
-    ARGS --target-env vulkan1.0 ${SHADER_SOURCES} --quiet
+    ARGS --target-env vulkan1.0  ${SHADER_SOURCES} --quiet -V 
     WORKING_DIRECTORY ${SHADERS_DIR}
     DEPENDS ${SHADERS_DIR} ${SHADER_SOURCES}
     COMMENT "Compiling Shaders"
@@ -17,6 +19,6 @@ function (add_shaders_target TARGET)
     )
   add_custom_target (${TARGET} DEPENDS ${SHADERS_DIR}/frag.spv ${SHADERS_DIR}/vert.spv)
   if(TARGET ${TARGET})
-    message(STATUS  "${TARGET} Shaders has compile sucessfully"  )
+    # message(STATUS  "${TARGET} Shaders has compile sucessfully"  )
   endif()
 endfunction ()
