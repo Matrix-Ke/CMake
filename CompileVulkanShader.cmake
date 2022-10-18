4a4d5f841695ae7a018f6fd7fba3a02b94077f8a
Matrix <2513084541@qq.com>
2022-10-18 15:18:31 +0800

    update

--- CompileVulkanShader.cmake
+++ CompileVulkanShader.cmake
@@ -22,3 +22,30 @@ function (add_shaders_target TARGET)
     # message(STATUS  "${TARGET} Shaders has compile sucessfully"  )
   endif()
 endfunction ()
+
+
+function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR GLSLANG_BIN)
+
+    set(working_dir "${CMAKE_CURRENT_SOURCE_DIR}/${GENERATED_DIR}/spv")
+    set(ALL_GENERATED_SPV_FILES "")
+
+    foreach(SHADER ${SHADERS})
+    # Prepare a header name and a global variable for this shader
+        get_filename_component(SHADER_NAME ${SHADER} NAME)
+
+        set(SPV_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${GENERATED_DIR}/spv/${SHADER_NAME}.spv")
+        
+        add_custom_command(
+            OUTPUT ${SPV_FILE}
+            COMMAND ${GLSLANG_BIN} -I${SHADER_INCLUDE_FOLDER} -V100 -o ${SPV_FILE} ${SHADER}
+            DEPENDS ${SHADER}
+            WORKING_DIRECTORY "${working_dir}")
+
+        list(APPEND ALL_GENERATED_SPV_FILES ${SPV_FILE})
+
+    endforeach()
+
+    # message(STATUS  ALL_GENERATED_SPV_FILES = ${ALL_GENERATED_SPV_FILES})
+    add_custom_target(${TARGET_NAME} DEPENDS ${ALL_GENERATED_SPV_FILES} SOURCES ${SHADERS})
+
+endfunction()
